0c1e6c1a19eacb4fa7a791628c46b1bb899a3a5d
Kouzema <kouzemataibaly>
2022-10-24 10:19:18 +0200

    Screens

---
+++ Screens/ConnectionScreen.js
@@ -0,0 +1,13 @@
+import { View, Text, Button } from 'react-native'
+
+export default function ConnectionScreen({ navigation }) {
+    return (
+      <View>
+        <Text>Connection Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    );
+   }
