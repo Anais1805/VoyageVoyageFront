6577a1d006d1c3a7f9e1dea00f3763edcf0a4606
ADJI-DOMORAK Alexia <alexia.adji@gmail.com>
2022-10-25 11:52:22 +0200

    al:3

---
+++ Screens/ReservedScreen.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet, Image, TouchableOpacity, TextInput } from 'react-native'
+
+
+export default function ReservedScreen({ navigation }) {
+
+    return (
+        <View>
+            <Text>ReservedScreen</Text>
+        </View>
+    )
+}
