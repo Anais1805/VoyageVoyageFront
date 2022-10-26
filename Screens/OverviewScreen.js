8c49f0feee7c904f9e6fcb6a0ebf6400d90527bb
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2022-10-26 16:06:51 +0200

    ReservationScreen Front+navig

---
+++ Screens/OverviewScreen.js
@@ -0,0 +1,19 @@
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    Pressable,
+    SafeAreaView,
+  } from "react-native";
+  import FontAwesome from "react-native-vector-icons/FontAwesome";
+  import { useState } from "react";
+
+  
+  export default function OverviewScreen({ navigation }) {
+    return(
+        <Text>Hello Overview</Text>
+    )
+  }
